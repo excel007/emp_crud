1fa360a13f7d29da73d92303d9659420ac936c69
excel007 <eak.ekkachai@gmail.com>
2023-10-26 14:46:00 +0700

    ok

--- app/emp_pool/api/route.tsx
+++ app/emp_pool/api/route.tsx
@@ -1,4 +1,4 @@
-import { NextRequest , NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import mysql, { PoolOptions } from 'mysql2/promise';
 import { mysqlaccess_pool } from "@/utils/dbpool";
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -17,34 +17,47 @@ type emp_type = {
   idjob: string,
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const pool = await mysql.createPool(mysqlaccess_pool);
 
   if (req.method === "GET") {
     const sql = 'SELECT * from employees'
     const [rows, fields] = await pool.query(sql);
-    
+
     // res.status(200).json({ employees: rows})
-    
+
     return NextResponse.json({ employees: rows });
   }
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   const pool = await mysql.createPool(mysqlaccess_pool);
   if (req.method === "POST") {
-
+    const body = await req.json()
     const employee: Partial<emp_type> = {};
-    employee.first_name = req.body.first_name
-    employee.first_name = req.body.last_name
-    employee.email = req.body.email
-    // const sql = 'insert into employees(`first_name`,`last_name`,`email`) values (?,?,?)'
+    employee.id = Math.floor(Math.random() * 1001) + 300,
+    employee.first_name = body.first_name
+    employee.last_name = body.last_name
+    employee.email = body.email
+    employee.phone_number = "650.501.2876"
+    employee.hire_date = "1998-07-01"
+    employee.salary = 9999
+    employee.commission_pct = 0
+    employee.iddepartment = 80
+    employee.idmanager = 101
+    employee.idjob = "IT_PROG"
+
+    // const sql = 'insert into employees(`id`,`first_name`,`last_name`,`email`,`idjob`) values (?,?,?,?,?)'
+    // const [rows, fields] = await pool.query(sql, [employee.id, employee.first_name, employee.last_name, employee.email, 'IT_PROG']);
     const sql = 'insert into employees values (?)'
-    const [rows, fields] = await pool.query(sql,[{employee}]);
+    const [rows, fields] = await pool.query(sql,[employee]);
+    console.log("=====Function POST=====")
+    console.log(employee)
+    console.log(sql)
+    console.log(typeof(employee))
     // res.status(200).json({ employees: rows})
     return NextResponse.json({ employees: rows });
   }
-
 }
 
 // export async function GET(request: any) {
--- app/emp_pool/create/page.tsx
+++ app/emp_pool/create/page.tsx
@@ -3,17 +3,30 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 
-export default function Page() {
-    const handleSubmit = (event:any) => {
+export default async function Page() {    
+    const handleSubmit = async (event: any) => {
         event.preventDefault()
         // console.log(event.target.first_name.value)
-        const data = {
-            "first_name" : event.target.first_name.value,
-            "last_name" : event.target.last_name.value,
-            "email" : event.target.email.value
+        const formData = {
+            "first_name": event.target.first_name.value,
+            "last_name": event.target.last_name.value,
+            "email": event.target.email.value
         }
-        // console.log(data)
-        fetch('http://')
+        console.log(formData)
+        const postData = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+        const add = await fetch('/emp_pool/api/', postData)
+        console.log(add)
+        // const res = await add.json()
+        // if (res.success > 0) {
+        //     console.log("Created complete")
+        // }
     }
     return (
         <div>
